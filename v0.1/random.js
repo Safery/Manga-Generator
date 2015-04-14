48223ffc9447171dc22f123ea319772205ee4f39
Safery Hossain <janingpower@yahoo.com>
2015-04-13 22:50:34 -0400

    Create random.js
    The following code will allow us to generate the random algorithm.

---
+++ v0.1/random.js
@@ -0,0 +1,20 @@
+// The following code will allow us to generate the random algorithm.
+
+<script type="text/javascript">
+var total_images = 12;
+var random_number = Math.floor((Math.random()*total_images));
+var random_img = new Array();
+random_img[0] = '<a href="#"><img src="1.png"></a>';
+random_img[1] = '<a href="#"><img src="2.png"></a>';
+random_img[2] = '<a href="#"><img src="3.png"></a>';
+random_img[3] = '<a href="#"><img src="4.png"></a>';
+random_img[4] = '<a href="#"><img src="5.png"></a>';
+random_img[5] = '<a href="#"><img src="6.png"></a>';
+random_img[6] = '<a href="#"><img src="7.png"></a>';
+random_img[7] = '<a href="#"><img src="8.png"></a>';
+random_img[8] = '<a href="#"><img src="9.png"></a>';
+random_img[9] = '<a href="#"><img src="10.png"></a>';
+random_img[10] = '<a href="#"><img src="11.png"></a>';
+random_img[11] = '<a href="#"><img src="12.png"></a>';
+document.write(random_img[random_number]);
+</script>
